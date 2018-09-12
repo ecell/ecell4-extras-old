b95be657dbfc1f7d4f53cb18c98d2914f821be07
Kozo Nishida <knishida@riken.jp>
2018-09-12 20:38:24 +0900

    chore: Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ecell4-extras',
+    version='0.1.0',
+    packages=['ecell4.util', 'ecell4.extra', 'ecell4.datasource'],
+    url='https://github.com/ecell/ecell4-extras',
+    license='the GNU General Public License v2',
+    author='Kazunari Kaizu',
+    author_email='kaizu@riken.jp',
+    description='An extra package other than the core part of the cell simulation platform E-Cell4'
+)
